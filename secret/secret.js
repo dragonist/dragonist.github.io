90dcbf66860067beed3206ea38c926c1ef6a47e4
dragonist <erin314@naver.com>
2015-06-14 02:24:04 +0900

    #secret

--- secret/secret.js
+++ secret/secret.js
@@ -1,8 +1,63 @@
-var nPage = 0;
-var nDuration = 700;
-var nDelay = 400;
-var nTmpNum = 0;
-var nVideoPage = 1;
-var bKeyDown = false;
-var htEvent = [];
-var sAttr = getAttr();
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+		sectionsColor: ['#31355c', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F'],
+		// sectionsColor: ['#7E8F7C', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F'],
+		verticalCentered : false,
+		resize : true,
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['First page', 'Second page', 'Third page', 'Fourth page', 'Fifth page', 'Sixth page', 'Last page'],
+		afterRender: function(){
+			$('video').get(0).play();	
+			console.log("video start");		
+			console.log("afterRender");	
+		},
+		afterResize: function(){
+			console.log("afterResize");
+		},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+        	console.log("onSlideLeave");
+        },
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+			console.log("afterSlideLoad");
+			console.log(anchorLink);
+			console.log(index);
+			console.log(slideAnchor);
+			console.log(slideindex);
+		}
+    });	
+});
+
+
+
+// window.addEventListener("load", function() {
+//     $('#fullpage').fullpage({
+// 		sectionsColor: ['#31355c', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F'],
+// 		// sectionsColor: ['#7E8F7C', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F'],
+// 		verticalCentered : false,
+// 		resize : true,
+// 		navigation: true,
+// 		navigationPosition: 'right',
+// 		navigationTooltips: ['First page', 'Second page', 'Third page', 'Fourth page', 'Fifth page', 'Sixth page', 'Last page'],
+// 		afterRender: function(){
+// 			$('video').get(0).play();	
+// 			console.log("video start");		
+// 			console.log("afterRender");	
+// 		},
+// 		afterResize: function(){
+// 			console.log("afterResize");
+// 		},
+//         onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+//         	console.log("onSlideLeave");
+//         },
+// 		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+// 			console.log("afterSlideLoad");
+// 			console.log(anchorLink);
+// 			console.log(index);
+// 			console.log(slideAnchor);
+// 			console.log(slideindex);
+// 		}
+//     });
+// 	// console.log("hello");
+// 	// $('video').get(0).play();
+// })
