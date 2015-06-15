267e3d8841a24fa7b620731c27513c0eadd1c24a
dragonist <erin314@naver.com>
2015-06-15 20:14:46 +0900

    #video good

--- secret/secret.js
+++ secret/secret.js
@@ -8,7 +8,9 @@ $(document).ready(function() {
 		navigationPosition: 'right',
 		navigationTooltips: ['First page', 'Second page', 'Third page', 'Fourth page', 'Fifth page', 'Sixth page', 'Last page'],
 		afterRender: function(){
-			$('video').get(0).play();	
+			$.fn.fullpage.reBuild();
+			$('video').get(0).play();
+			$('video').get(1).play();	
 			console.log("video start");		
 			console.log("afterRender");	
 		},
@@ -28,36 +30,8 @@ $(document).ready(function() {
     });	
 });
 
-
-
 // window.addEventListener("load", function() {
-//     $('#fullpage').fullpage({
-// 		sectionsColor: ['#31355c', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F'],
-// 		// sectionsColor: ['#7E8F7C', '#1BBC9B', '#7E8F7C', '#C63D0F', '#1BBC9B', '#7E8F7C', '#C63D0F'],
-// 		verticalCentered : false,
-// 		resize : true,
-// 		navigation: true,
-// 		navigationPosition: 'right',
-// 		navigationTooltips: ['First page', 'Second page', 'Third page', 'Fourth page', 'Fifth page', 'Sixth page', 'Last page'],
-// 		afterRender: function(){
-// 			$('video').get(0).play();	
-// 			console.log("video start");		
-// 			console.log("afterRender");	
-// 		},
-// 		afterResize: function(){
-// 			console.log("afterResize");
-// 		},
-//         onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
-//         	console.log("onSlideLeave");
-//         },
-// 		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
-// 			console.log("afterSlideLoad");
-// 			console.log(anchorLink);
-// 			console.log(index);
-// 			console.log(slideAnchor);
-// 			console.log(slideindex);
-// 		}
-//     });
-// 	// console.log("hello");
-// 	// $('video').get(0).play();
+
 // })
+
+// 7662a7
